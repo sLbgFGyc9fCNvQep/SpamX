e983e66f6e158d11966be10881bff2851aa0c0b8
Xoticxd <99335431+sLbgFGyc9fCNvQep@users.noreply.github.com>
2023-06-26 14:37:25 +0530

    Update _SpamX.py

--- SpamX/modules/for_id/_SpamX.py
+++ SpamX/modules/for_id/_SpamX.py
@@ -179,6 +179,7 @@ async def help_(_, message: Message):
 
 @Client.on_message(filters.chat(AUTO_REACT) & filters.all)
 async def auto_react(_, message: Message):
-   emoj = random.choice(EMOJI_LIST)if message.from_user.username not in ["MissRose_bot", "SuzuneSuperBot"]:
+   emoj = random.choice(EMOJI_LIST)
+    if message.from_user.username not in ["MissRose_bot", "SuzuneSuperBot"]:
    return
    await message.react(emoji=emoj, big=True)
